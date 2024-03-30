b17a244f6e70014d4b87e39eff656eb5d7b07099
Kristina140699 <Kristina.11915169@lpu.in>
2024-03-30 23:00:49 +0530

    Created the Customer & Dealer Segregation feature.

---
+++ entry.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  </head>
+  <body>
+   <?php require 'partials\_nav.php' ?>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+  <img style="width: 48%; height:40%;" src="public\carRental.jpeg" class="card-img-top" alt="Car Rental">
+  <a href="/internshala-assignment/login.php">
+  <button style="width: 150px; height: 100px; margin:10%" type="button" class="btn btn-success">
+  Join as a Customers</button></a>
+  
+  <a href="/internshala-assignment/login.php" style="text-decoration: none">
+  <button style="width: 150px; height: 100px;" type="button" class="btn btn-warning">Join as a Dealer</button>
+  </a>
+
+<div style="margin:10%">
+</div>
+  </body>
+</html>
